b4bd1b765a5b39fb772f879684b9def78b8c9605
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-07 22:07:26 -0300

    Intereses

---
+++ Interes.vb/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+
+        Dim m, r, t, i As Integer
+
+        Console.Write("Ingrese Monto: ")
+        m = Console.ReadLine()
+        Console.Write("Ingrese Interes: ")
+        r = Console.ReadLine()
+        Console.Write("Ingrese dias: ")
+        t = Console.ReadLine()
+
+        i = m * r * t / 360 * 100
+
+        Console.WriteLine("Intereses totales: " & i)
+
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
---
+++ README.md
@@ -0,0 +1,2 @@
+# Intereses.vb
+ 
